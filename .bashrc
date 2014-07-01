d988c41c5e3a6c5b78e4d470fd3b786950188242
bestape <user@bestape.net>
2014-07-01 23:16:40 +0000

    made update without approval and option to reboot after setup

--- .bashrc
+++ .bashrc
@@ -232,7 +232,7 @@ fi
 alias gitAB="git log -p --decorate --color"
 alias getApt="sudo apt-get install"
 alias getDeb="sudo dpkg -i"
-alias update="sudo apt-get update && sudo apt-get upgrade"
+alias update="sudo apt-get -y update && sudo apt-get -y upgrade"
 alias updateBash="source .bashrc"
 alias editSsh="em ~/.ssh/config"
 alias aliases="grep 'alias ' ~/.bashrc ~/.bashrc_unique"
--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 nodeVer=$(cat $DIR/.nvmrc)
 ln -sb $DIR/.nvmrc ~
 ln -sb $DIR/.screenrc ~
-cp $DIR/.bash_profile.proto $DIR/.bashrc_profile
+cp $DIR/.bash_profile.proto $DIR/.bash_profile
 ln -sb $DIR/.bash_profile ~
 ln -sb $DIR/.bashrc ~
 cp $DIR/.bashrc_unique.proto $DIR/.bashrc_unique
@@ -12,18 +12,17 @@ ln -sb $DIR/.bashrc_unique ~
 cp $DIR/.backuprc.proto $DIR/.backuprc
 ln -sb $DIR/.backuprc ~
 ln -sf $DIR/.emacs.d ~
-echo '
---------------- git options setup ----------------
+echo "--------------- git options setup ----------------
 ------------------ part 1 of 2 -------------------
 please input your git user.name and press [ENTER]:
-If you want to skip, leave blank and press [ENTER]:'
+If you want to skip, leave blank and press [ENTER]:"
 read userName
 if [ $userName ]; then
     git config --global user.name $userName
 fi
-echo '------------------ part 2 of 2 -------------------'
-echo 'please input your git user.email and press [ENTER]:'
-echo 'If you want to skip, leave blank press [ENTER]:'
+echo "------------------ part 2 of 2 -------------------
+please input your git user.email and press [ENTER]:
+If you want to skip, leave blank press [ENTER]:"
 read userEmail
 if [ $userEmail ]; then	
     git config --global user.email $userEmail
@@ -32,9 +31,22 @@ curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | sh
 source ~/.nvm/nvm.sh
 nvm install $nodeVer
 node $DIR/setup.js
-source .bashrc
-echo '
+sudo apt-get -y update && sudo apt-get -y upgrade
+echo "
+--------------- fin setup notice -----------------
 Setup is now complete.
 Please load a new login bash shell to start using all the features.
-You can load a new login bash shell by logging in and out of this operating system.'
-exit 0
+You can load a new login bash shell by logging in and out of this operating system.
+Alternatively, you can reboot the entire operating system.
+
+--------------- fin setup options ----------------
+Would you like to reboot the system now?
+If so, type yes and press [ENTER].
+Otherwise, leave blank and press [ENTER] to exit this setup."
+read rebootQ
+if [ $rebootQ ] && [ $rebootQ == "yes" ]; then
+	sudo reboot
+else
+	source ~/.bashrc
+	exit 0
+fi
